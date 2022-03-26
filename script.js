73743af3448b187b973babf01faa08a22c5cb588
Francisco Garat <panchogarat56@gmail.com>
2022-03-26 16:22:40 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//operaciones basicas
+
+const sumar = function(a, b) {
+    return a + b;    
+};
+  
+const restar = function(a, b) {
+      return a - b;
+};
+
+  const multiplicar = function(a, b){
+      return a * b;
+};
+
+  const dividir = function(a, b){
+    return a / b;
+};
+//FUNCION OPERATIVA
+  const operar = function(operacion, a, b){
+      return operacion(a, b);
+};
