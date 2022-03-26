280e3c22c2a3dcd7fd10ff9ade88349b179e91b1
Francisco Garat <panchogarat56@gmail.com>
2022-03-26 19:32:11 -0300

    paso 4

--- script.js
+++ script.js
@@ -17,5 +17,30 @@ const restar = function(a, b) {
 };
 //FUNCION OPERATIVA
   const operar = function(operacion, a, b){
-      return operacion(a, b);
+      
+    return operacion(a, b);
 };
+
+const numbers = Array.from(document.querySelectorAll('button'));
+console.log(numbers);
+
+const display = document.getElementById("display");
+
+
+// numbers[0].addEventListener("click", displayNumbers(0));
+
+// numbers.forEach(number => { 
+//     number.addEventListener("click", displayNumbers(number.innerHTML));
+// });
+
+function clearNumbers() {
+    display.innerHTML = "";
+}
+
+function displayNumbers(evento){
+display.innerHTML += evento;
+let displayValue = display.innerHTML;
+return displayValue;
+};
+
+
